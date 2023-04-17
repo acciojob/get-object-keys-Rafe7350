cc24ee50d67aa94bb8586bbbca27456f08b3a934
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-17 11:18:01 +0530

    add

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// create the student object
+const student = {
+    name: "John Doe",
+  };
+  
+  // add a getKeys() method to the Object prototype
+  Object.prototype.getKeys = function() {
+    return Object.keys(this);
+  };
+  
+  // test the getKeys() method on the student object
+  console.log(student.getKeys()); // output: ["name"]
+  
